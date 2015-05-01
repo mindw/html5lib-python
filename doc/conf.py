9684c2dbfc1f92e83a45f60b6f7d7f040c8323ce
Gabi Davar <grizzly.nyo@gmail.com>
2015-05-01 11:12:10 +0300

    docs

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,12 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -170,7 +175,7 @@ html_theme = 'default'
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'html5libdoc'
+#htmlhelp_basename = 'html5libdoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
